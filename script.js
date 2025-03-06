546f4d3f4864e596607ea63cb57944f1fce1568a
lokejer <lokejer@gmail.com>
2025-03-06 16:37:27 +0800

    Projects screen and footer made responsive, added typewriter effect for introductory text and cool border animation for Projects button

--- script.js
+++ script.js
@@ -11,22 +11,53 @@ logo.onclick = function() {
   window.location.href = "index.html"
 }
 
-const titleVariations = [
-  "Hi! I'm Jerome.",
-  "Hi! I'm lokejer@gmail.com",
-  "Hi! I'm @jero00o"
-
-  // "你好! I'm Jerome.",
-]
-
-let index = 0
-function rotateIntroText() {
-  const titleText = document.getElementById("title-text")
-  
-  titleText.innerText = titleVariations[index]
-
-  index = (index + 1) % titleVariations.length
+document.getElementById("separator-line").onclick = function() {
+  window.location.href =
+		"https://sopossible.sp.edu.sg/courses/schools/soc/applied-ai-analytics"
 }
 
-// run function every 2s indefinitely
-setInterval(rotateIntroText, 2400)
+const titleVariations = [
+	"hi! i'm Jerome...",
+  "i do machine learning...",
+  "and full-stack development!",
+	"email: lokejer@gmail.com;",
+]
+
+// INTRO SECTION TYPEWRITER EFFECT 
+let index = 0
+let char_index = 0
+let isDeleting = false
+
+const titleText = document.getElementById("title-text")
+
+function typewriterEffect() {
+	const currentText = titleVariations[index]
+
+	if (!isDeleting) {
+		// Add one character at a time
+		titleText.innerText = currentText.slice(0, char_index + 1)
+		char_index++
+
+		// If the entire word is typed, pause and start deleting
+		if (char_index === currentText.length) {
+			isDeleting = true
+			setTimeout(typewriterEffect, 1600) // Pause for a while before deleting
+			return
+		}
+	} else {
+
+		// delete the entire word after typing
+		titleText.innerText = ""
+		char_index = 0
+
+		// after deleting, move to the next variation
+		isDeleting = false
+		index = (index + 1) % titleVariations.length
+	}
+
+	// adjust typing speed
+	let speed = isDeleting ? 140 : 160
+	setTimeout(typewriterEffect, speed)
+}
+
+typewriterEffect()
