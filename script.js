e46a0934536009a2f3c43b9a445338673fdb8eb6
lokejer <lokejer@gmail.com>
2025-03-06 09:59:31 +0800

    Added "name carousel", gradient border & glow effect to buttons and fixed .logo face shrinking on hover

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const logo = document.querySelector(".logo")
 logo.onclick = function() {
   window.location.href = "index.html"
 }
+
+const titleVariations = [
+  "Hi! I'm Jerome.",
+  "Hi! I'm lokejer@gmail.com",
+  "Hi! I'm @jero00o"
+
+  // "你好! I'm Jerome.",
+]
+
+let index = 0
+function rotateIntroText() {
+  const titleText = document.getElementById("title-text")
+  
+  titleText.innerText = titleVariations[index]
+
+  index = (index + 1) % titleVariations.length
+}
+
+// run function every 2s indefinitely
+setInterval(rotateIntroText, 2400)
