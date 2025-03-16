3a5b0e7ca8e0f70d23d4a4adabfe7238e44ef892
lokejer <lokejer@gmail.com>
2025-03-16 21:59:49 +0800

    Completely Revamped Site Theme

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-function showSidebar() {
-  document.querySelector(".sidebar").style.display = "block"
-}
+const cursor = document.getElementById("cursor")
 
-function hideSidebar() {
-  document.querySelector(".sidebar").style.display = "none"
-}
+document.addEventListener("mousemove", (e) => {
+	cursor.style.left = `${e.clientX}px`
+	cursor.style.top = `${e.clientY}px`
+})
 
-const logo = document.querySelector(".logo")
-logo.onclick = function() {
-  window.location.href = "index.html"
-}
-
-document.getElementById("separator-line").onclick = function() {
-  window.location.href =
+document.getElementById("separator-line").onclick = function () {
+	window.location.href =
 		"https://sopossible.sp.edu.sg/courses/schools/soc/applied-ai-analytics"
 }
 
 window.addEventListener("scroll", () => {
-  let arrow = document.getElementById("scroll-indicator")
-  let target = document.getElementById("intro-btn").offsetTop
-  let scrollPosition = window.scrollY + window.innerHeight
+	let arrow = document.getElementById("scroll-indicator")
+	let target = document.getElementById("sp-logo").offsetTop
+	let scrollPosition = window.scrollY + window.innerHeight
 
-  if (scrollPosition >= target) {
-    arrow.classList.add("hidden")
-  } else {
-    arrow.classList.remove("hidden")
-  }
+	if (scrollPosition >= target) {
+		arrow.classList.add("hidden")
+	} else {
+		arrow.classList.remove("hidden")
+	}
 })
 
 const titleVariations = [
-	"hi! i'm Jerome...",
-  "i specialise in AI / MLOps,",
-  "& full-stack development.",
+	"> jerome loke",
+	"> MLOps",
+	"> Web Development",
 	"> lokejer@gmail.com",
 ]
 
-// INTRO SECTION TYPEWRITER EFFECT 
+// INTRO SECTION TYPEWRITER EFFECT
 let index = 0
 let char_index = 0
 let isDeleting = false
@@ -53,22 +47,21 @@ function typewriterEffect() {
 		// If the entire word is typed, pause and start deleting
 		if (char_index === currentText.length) {
 			isDeleting = true
-			setTimeout(typewriterEffect, (index === 3 ? 5000 : 1400)) // pause before deleting
+			setTimeout(typewriterEffect, index === 3 ? 5000 : 1400) // pause before deleting
 			return
 		}
 	} else {
+		titleText.innerText = currentText.slice(0, char_index - 1)
+		char_index--
 
-		// delete the entire word after typing
-		titleText.innerText = ""
-		char_index = 0
-
-		// after deleting, move to the next variation
-		isDeleting = false
-		index = (index + 1) % titleVariations.length
+		if (char_index === 1) {
+			isDeleting = false
+			index = (index + 1) % titleVariations.length
+		}
 	}
 
 	// adjust typing speed
-	let speed = isDeleting ? 20 : 120
+	let speed = isDeleting ? 60 : 100
 	setTimeout(typewriterEffect, speed)
 }
 
