11d27a21d21a4e3f5d15b995ae770e8323dbf4ec
lokejer <lokejer@gmail.com>
2025-03-20 21:07:16 +0800

    Added Projects Carousel and fixed Navbar in Contacts page (originally projects page)

--- script.js
+++ script.js
@@ -42,3 +42,47 @@ function typewriterEffect() {
 }
 
 typewriterEffect()
+
+// IMAGE CAROUSEL PARALLAX EFFECT
+const track = document.getElementById("image-track")
+
+window.onmousedown = (e) => {
+	track.dataset.mouseDownAt = e.clientX
+}
+
+window.onmouseup = () => {
+	track.dataset.mouseDownAt = "0" // reset on mouse up
+	track.dataset.prevPercentage = nextPercentage
+}
+
+window.onmousemove = (e) => {
+	if (track.dataset.mouseDownAt === "0") return
+
+	const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+		maxDelta = window.innerWidth / 2
+
+	// track last mouse position
+	const percentage = (mouseDelta / maxDelta) * -100
+	nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage
+
+	// **clamp the value within -100 to 0**
+	nextPercentage = Math.max(Math.min(nextPercentage, 0), -100)
+
+	track.dataset.percentage = nextPercentage
+
+	track.animate(
+		{
+			transform: `translate(${nextPercentage}%, -50%)`,
+		},
+		{ duration: 10000, fill: "forwards" }
+	)
+
+	for (const image of track.getElementsByClassName("image")) {
+		image.animate(
+			{
+				objectPosition: `${nextPercentage + 100}% 50%`,
+			},
+			{ duration: 10000, fill: "forwards" }
+		)
+	}
+}
